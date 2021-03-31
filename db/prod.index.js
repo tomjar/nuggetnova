15eb9e25e26e6ad15830db66747fba6b9a0327d1
Thomas Reese <cyclemadmot@gmail.com>
2021-03-30 20:50:20 -0500

    updates to prod connection

--- db/prod.index.js
+++ db/prod.index.js
@@ -1,8 +1,20 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
+}
+
+const pool = new Pool(config)
+pool.connect().then(client => {
+  console.log('connected');
+})
+.catch(err => console.error('error connecting', err.stack))
+.then(() => pool.end)
 
 module.exports = {
   query: (text, params, callback) => {
